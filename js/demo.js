c5d6effccc99925d8b7fdb855c7652a71568f117
vashisthshailesh <43761254+vashisthshailesh@users.noreply.github.com>
2020-07-09 23:56:15 +0530

    Add files via upload
    primary  changes

---
+++ js/demo.js
@@ -0,0 +1,12 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+  particleground(document.getElementById('particles'), {
+    dotColor: '#5cbdaa',
+    lineColor: '#5cbdaa'
+  });
+  var intro = document.getElementById('intro');
+  intro.style.marginTop = - intro.offsetHeight/1.7  + 'px';
+}, false);
+
+
+
